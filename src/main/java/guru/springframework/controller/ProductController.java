a29da0efa2ef2b4b6e349e56c8513bc3ec1d0009
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-09-28 00:09:46 +0300

    Use the Thymeleaf image tag

--- src/main/java/guru/springframework/controller/ProductController.java
+++ src/main/java/guru/springframework/controller/ProductController.java
@@ -1,10 +1,12 @@
 package guru.springframework.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
 
+    @RequestMapping("product")
     public String getProduct() {
         return "product";
     }

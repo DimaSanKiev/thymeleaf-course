171b357c14172fff2d80ffd60260a5582aeb00ff
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-09-27 22:12:47 +0300

    Add two controller classes

---
+++ src/main/java/guru/springframework/controller/ProductController.java
@@ -0,0 +1,11 @@
+package guru.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+
+    public String getProduct() {
+        return "product";
+    }
+}

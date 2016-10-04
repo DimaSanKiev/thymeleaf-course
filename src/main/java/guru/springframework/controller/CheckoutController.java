3f968f984d21b27b4cc8b78dc26eb577c3a79a5c
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-05 01:08:27 +0300

    Add checkout Thymeleaf forms

--- src/main/java/guru/springframework/controller/CheckoutController.java
+++ src/main/java/guru/springframework/controller/CheckoutController.java
@@ -2,6 +2,7 @@ package guru.springframework.controller;
 
 import guru.springframework.command.CheckoutCommand;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,16 +12,17 @@ import javax.validation.Valid;
 @Controller
 public class CheckoutController {
 
-    @RequestMapping("checkout")
-    private String checkoutForm() {
-        return "checkoutForm";
+    @RequestMapping("/checkout")
+    private String checkoutForm(Model model) {
+        model.addAttribute("checkoutCommand", new CheckoutCommand());
+        return "checkoutform";
     }
 
     @RequestMapping(value = "/docheckout", method = RequestMethod.POST)
     public String doCheckout(@Valid CheckoutCommand checkoutCommand, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "checkoutForm";
+            return "checkoutform";
         }
-        return "checkoutComplete";
+        return "checkoutcomplete";
     }
 }

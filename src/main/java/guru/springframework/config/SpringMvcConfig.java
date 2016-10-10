7b2f02ced074ec9169ed13583716a509343aeb58
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-10 22:21:07 +0300

    Add Spring Security configuration

--- src/main/java/guru/springframework/config/SpringMvcConfig.java
+++ src/main/java/guru/springframework/config/SpringMvcConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @Configuration
-public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public LocaleResolver localeResolver() {

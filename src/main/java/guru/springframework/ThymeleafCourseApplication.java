24ee0db664d75f36ef2d2199fcc069160ccd4c75
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-10-13 00:39:03 +0300

    war deploying configuration

--- src/main/java/guru/springframework/ThymeleafCourseApplication.java
+++ src/main/java/guru/springframework/ThymeleafCourseApplication.java
@@ -2,11 +2,12 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThymeleafCourseApplication {
+public class ThymeleafCourseApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThymeleafCourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafCourseApplication.class, args);
+    }
 }
